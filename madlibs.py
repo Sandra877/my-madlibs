17723778b20051358888398c2262cfd96b94f8cf
Sandra877 <110770016+Sandra877@users.noreply.github.com>
2022-08-10 16:03:04 +0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+verb1 = input("Verb: ")
+noun1 = input("Noun: ")
+verbing1= input("Verb with ing: ")
+verbing2 = input("Verb with ing: ")
+pastverb1 = input("Verb in past tense ending ed: ")
+noun2 = input("Noun: ")
+adj1 = input("Adjective: ")
+noun3 = input("Noun: ")
+adj2 = input("Adjective: ")
+noun4 = input("Noun: ")
+adj3 = input("Adjective: ")
+
+
+
+
+
+madlib = (f"I bet everyone in the world loves to {verb1}. So my {noun1} started {verbing1} today at 2am and i was \
+already {verbing2}. I just {pastverb1} at him before proceeding to go to the {noun2} to get my {adj1} {noun3} done by\
+ my {adj2} {noun4}. It was honestly a really {adj3} experience.")
+
+print(madlib)
